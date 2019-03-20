88104638f50bc1ca51e462e54b7409f2b11243f3
Kasey Tiller <kntiller@ccicustom.com>
2019-03-20 12:55:53 -0400

    adding final settings for debug toolbar

--- apile/urls.py
+++ apile/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
@@ -31,6 +30,12 @@ urlpatterns += [
 
 urlpatterns += static (settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-# urlpatterns =[
-#     path
-# ]
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+
+        # For django versions before 2.0:
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+    ] + urlpatterns
